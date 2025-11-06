8e8517d70fc9a2a3cc41c797e87f3406bcfbbce3
Julian <julian.t.preston@gmail.com>
2025-11-06 12:55:46 -0800

    0.0.48b

--- build.sh
+++ build.sh
@@ -11,17 +11,17 @@ gh pr create
 clear
 echo "Building source..."
 mkdir ~/elecree_binaries/$VERSION
-cd ~/elecree-binaries/$VERSION
+cd ~/elecree_binaries/$VERSION
 mkdir lin64 win64 lin32 win32
 cd $PROJECTDIR
-godot3 --export "Linux/X11" ~/elecree-binaries/$VERSION/lin64/elecree-$VERSION-lin64.x86-64
-godot3 --export "Windows Desktop" ~/elecree-binaries/$VERSION/win64/elecree-$VERSION-win64.exe
-godot3 --export "Linux 32-Bit" ~/elecree-binaries/$VERSION/lin32/elecree-$VERSION-lin32.x86
-godot3 --export "Windows 32-Bit" ~/elecree-binaries/$VERSION/win32/elecree-$VERSION-win32.exe
-godot3 --export "Mac OSX" ~/elecree-binaries/$VERSION/elecree-$VERSION-mac.zip
+godot3 --export "Linux/X11" ~/elecree_binaries/$VERSION/lin64/elecree-$VERSION-lin64.x86-64
+godot3 --export "Windows Desktop" ~/elecree_binaries/$VERSION/win64/elecree-$VERSION-win64.exe
+godot3 --export "Linux 32-Bit" ~/elecree_binaries/$VERSION/lin32/elecree-$VERSION-lin32.x86
+godot3 --export "Windows 32-Bit" ~/elecree_binaries/$VERSION/win32/elecree-$VERSION-win32.exe
+godot3 --export "Mac OSX" ~/elecree_binaries/$VERSION/elecree-$VERSION-mac.zip
 clear
 echo "Creating Binaries..."
-cd ~/elecree-binaries/$VERSION
+cd ~/elecree_binaries/$VERSION
 zip elecree-$VERSION-lin64.zip lin64/*
 zip elecree-$VERSION-win64.zip win64/*
 zip elecree-$VERSION-lin32.zip lin32/*
