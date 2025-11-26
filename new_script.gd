b359abab7def200427f75b865b6b75f738eb9431
Julian <julian.t.preston@gmail.com>
2025-11-26 14:46:21 -0800

    0.0.56

--- new_script.gd
+++ new_script.gd
@@ -6,7 +6,7 @@ onready var data = load("res://creatures.tres")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize() # No more predictable seeding!
-	print("==[ Elecree v0.0.55 ]==")
+	print("==[ Elecree v0.0.56 ]==")
 	global.cutscenePlaying = true
 	print(data.attack_list)
 
